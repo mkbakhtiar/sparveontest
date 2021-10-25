30481bf9549cc2242bddc83cb12a6228c4e7531e
Muhammad Khoirul Bakhtiar <mkbakhtiar.new@gmail.com>
2021-10-25 23:18:00 +0700

    First File React

---
+++ src/component/DashboardComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DashboardComponent() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
+
+export default DashboardComponent
---
+++ src/component/MenuComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MenuComponent() {
+    return (
+        <div>
+            <h1>Menu</h1>
+        </div>
+    )
+}
+
+export default MenuComponent
